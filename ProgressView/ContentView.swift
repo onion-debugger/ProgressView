377f17cf2f666f5bbbdd5233ebb729685e8b82c6
onion_debugger <oshiole94@gmail.com>
2025-07-15 18:01:03 +0100

    Initial Commit

---
+++ ProgressView/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  ProgressView
+//
+//  Created by onion_debugger on 14/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showProgressView = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            if showProgressView {
+                ProgressView("Loading")
+                    .tint(.purple) // change the color of the progress
+                    .scaleEffect(2.5) // increase size of the progress view
+            }
+            
+            Spacer()
+            
+            Button("Toggle Progress View") {
+                showProgressView.toggle()
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by onion_debugger on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
